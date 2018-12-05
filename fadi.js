f15e2913075da03d2aff0e3ab5e41b3ca4e14ada
fadirs <45557717+fadirs@users.noreply.github.com>
2018-12-05 12:55:21 -0800

    Update fadi.js

--- fadi.js
+++ fadi.js
@@ -255,10 +255,10 @@ restService.post("/slack-test", function(req, res) {
   });
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log("Listening on " + port);
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+  //  console.log("Listening on " + port);
 
-//restService.listen(process.env.PORT || 8000, function() {
-  //console.log("Server up and listening");
-//});
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
