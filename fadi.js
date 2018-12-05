5e5e5eca9fc676f60ad365f09deedccba563567e
fadirs <45557717+fadirs@users.noreply.github.com>
2018-12-05 09:36:10 -0800

    Update fadi.js

--- fadi.js
+++ fadi.js
@@ -275,6 +275,6 @@ app.get('/', function(request, response) {
 
 
 
-restService.listen(process.env.PORT || 8000, function() {
+restService.listen(process.env.PORT || 5000, function() {
   console.log("Server up and listening");
 });
