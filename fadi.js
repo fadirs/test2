c01890589dc92daacac3143c6cc94033db17da68
fadirs <45557717+fadirs@users.noreply.github.com>
2018-12-05 04:51:22 -0800

    Update fadi.js

--- fadi.js
+++ fadi.js
@@ -43,7 +43,7 @@ app.get('/', function(request, response) {
             console.log('error: ', err);
             throw err;
         }
-        response.send('Hello World!!!! fadi!!!!');
+        response.send(['Hello World!!!! fadi!!!!', rows]);
     });
 });
 
